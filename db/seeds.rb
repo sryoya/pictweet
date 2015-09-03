eda1620092ddafbdab3754b4103c7a9c491b6725
関野 涼也 <sryoya0814@gmail.com>
2015-09-03 19:41:53 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require "csv"
 tweets_csv = CSV.readlines("db/tweets.csv")
 tweets_csv.shift
 tweets_csv.each do |row|
-  Tweet.create(name: row[1], text: row[2], image: row[3], created_at: row[4], updated_at: row[5], user_id: row[6])
+  Tweet.create(text: row[1], image: row[2],user_id: row[5])
 end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
